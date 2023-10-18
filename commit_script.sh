a751df30c7fabf53c1acd58db8074948abdf3085
jonathanbcsouza <jonathanbcsouza@gmail.com>
2023-10-18 13:22:15 +1300

    feat: add contribution type details in commit messages

--- commit_script.sh
+++ commit_script.sh
@@ -5,29 +5,32 @@ if [ ! -f contributions.txt ]; then
 fi
 
 # Extract dates and times from lines starting with "Date: " in github_contributions.patch
-dates=($(grep '^Date: ' gitlab_contributions.patch | cut -d' ' -f2))
-times=($(grep '^Date: ' gitlab_contributions.patch | cut -d' ' -f3))
-commit_message="Migrate contributions"
+file_name="gitlab_contributions.patch"
+
+dates=($(grep '^Date: ' $file_name | cut -d' ' -f2))
+times=($(grep '^Date: ' $file_name | cut -d' ' -f3))
+contr_type=($(awk -F'[()]' '/^Subject:/ {split($2,a,"-"); print a[1]}' $file_name))
 source="Gitlab"
 origin="GitHub"
+commit_message="docs: import $contr_type contribution data from $source"
 
 combined_dates=()
 for i in "${!dates[@]}"; do
     combined_dates+=("${dates[$i]} ${times[$i]}")
-done
+done                              
 
 for line in "${combined_dates[@]}"; do
 
   # Add the contribution to the contributions.txt file
-  echo "$commit_message from $source | $line" >> contributions.txt
+  echo "$commit_message | $line" >> contributions.txt
 
-  git add contributions.txt
-  git commit -m "task: migrate contributions from $source on $line" --date="$line"
+  # git add contributions.txt
+  # git commit -m "$line" --date="$line"
 
-  echo "Executed: git commit -m \"$commit_message from $source | $line\" --date=\"$line\""
+  echo "Executed: git commit -m \"$commit_message | $line\" --date=\"$line\""
 
 done
 
-git push origin main
+# git push origin main
 
 echo "Success! All contributions from $source have been successfully migrated and will soon be visible on your $origin profile."
