9433bfb6cf1463145dd2d62d87dd39e347887b60
jonathanbcsouza <jonathanbcsouza@gmail.com>
2023-10-19 20:28:42 +1300

    style: add styling to prompt messages

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
+import time
 import subprocess
+import sys
+
+set_default_prompt_color = "\033[0m"
+set_green_color = "\033[32m"
+set_red_color = "\033[31m"
 
 def scrape_gitlab_activity_page():
     print("Scraping GitLab Activity Page...")
-    subprocess.run(["python", "contributions_page_scraper.py"])
-    print("Scraping completed.")
+    subprocess.run([python_executable, "contributions_page_scraper.py"])
+    print(f"{set_green_color}Scraping completed.{set_default_prompt_color}\n")
 
 def run_data_parser():
     print("Running Data Parser...")
-    subprocess.run(["python", "data_parser.py"])
-    print("Data parsing completed.")
+    subprocess.run([python_executable, "data_parser.py"])
+    print(f"{set_green_color}Data parsing completed.{set_default_prompt_color}\n")
 
 def run_contributions_register():
     print("Running Contributions Register...")
-    subprocess.run(["python", "contributions_register.py"])
-    print("Contributions registration completed.")
+    subprocess.run([python_executable, "contributions_register.py"])
+    print(f"{set_green_color}Contributions registration completed.{set_default_prompt_color}\n")  # Added "\n" for a line break
+    
 
-def main():
+if __name__ == "__main__":
+    try:
+        # Use the which command to find the path to Python executable
+        python_executable = subprocess.check_output(["which", "python3"]).decode().strip()
+    except subprocess.CalledProcessError:
+        print(f"{set_red_color}Error: Python3 not found.{set_default_prompt_color}\n")
+        sys.exit(1)
+
     scrape_gitlab_activity_page()
     run_data_parser()
     run_contributions_register()
-
-if __name__ == "__main__":
-    main()
